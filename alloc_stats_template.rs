ca89c07b36d5925bec113186f7d40bb0c034dc6c
panicbit <panicbit.dev@gmail.com>
2018-10-09 08:39:25 +0200

    Add dealloc stats

--- alloc_stats_template.rs
+++ alloc_stats_template.rs
@@ -17,7 +17,9 @@ fn main() {{
 static __STAT_ALLOC: self::__stat_alloc::StatAlloc = self::__stat_alloc::StatAlloc {{
     allocator: std::alloc::System,
     num_allocs: std::sync::atomic::AtomicUsize::new(0),
+    num_deallocs: std::sync::atomic::AtomicUsize::new(0),
     bytes_allocd: std::sync::atomic::AtomicUsize::new(0),
+    bytes_deallocd: std::sync::atomic::AtomicUsize::new(0),
 }};
 
 mod __stat_alloc {{
@@ -27,24 +29,37 @@ mod __stat_alloc {{
     pub struct StatAlloc {{
         pub allocator: System,
         pub num_allocs: AtomicUsize,
+        pub num_deallocs: AtomicUsize,
         pub bytes_allocd: AtomicUsize,
+        pub bytes_deallocd: AtomicUsize,
     }}
     
     impl StatAlloc {{
         pub fn print_stats(&self) {{
             let num_allocs = self.num_allocs.load(SeqCst);
+            let num_deallocs = self.num_deallocs.load(SeqCst);
             let bytes_allocd = self.bytes_allocd.load(SeqCst);
-            println!("allocs: {{}}, bytes: {{}}", num_allocs, bytes_allocd);
+            let bytes_deallocd = self.bytes_deallocd.load(SeqCst);
+            println!("  #allocs: {{}},   allocated bytes: {{}}", num_allocs, bytes_allocd);
+            println!("#deallocs: {{}}, deallocated bytes: {{}}", num_deallocs, bytes_deallocd);
+            println!("lost bytes: {{}}", bytes_allocd as isize - bytes_deallocd as isize);
         }}
         
         pub fn reset(&self) {{
             self.num_allocs.store(0, SeqCst);
+            self.num_deallocs.store(0, SeqCst);
             self.bytes_allocd.store(0, SeqCst);
+            self.bytes_deallocd.store(0, SeqCst);
         }}
         
         fn count_alloc(&self, size: usize) {{
             self.num_allocs.fetch_add(1, SeqCst);
             self.bytes_allocd.fetch_add(size, SeqCst);
+        }}
+
+        fn count_dealloc(&self, size: usize) {{
+            self.num_deallocs.fetch_add(1, SeqCst);
+            self.bytes_deallocd.fetch_add(size, SeqCst);
         }}
     }}
     
@@ -55,6 +70,7 @@ mod __stat_alloc {{
         }}
     
         unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {{
+            self.count_dealloc(layout.size());
             self.allocator.dealloc(ptr, layout)
         }}
     
@@ -64,6 +80,7 @@ mod __stat_alloc {{
         }}
     
         unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {{
+            self.count_dealloc(layout.size());
             self.count_alloc(new_size);
             self.allocator.realloc(ptr, layout, new_size)
         }}

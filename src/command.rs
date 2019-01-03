c0b43c9d95c6a70dedf6fb7c1e3b6f8132a776ec
panicbit <panicbit.dev@gmail.com>
2019-01-03 05:04:55 +0100

    Add raw_args to command struct

--- src/command.rs
+++ src/command.rs
@@ -1,19 +1,26 @@
 pub struct Command<'msg> {
     name: &'msg str,
     args: Vec<&'msg str>,
+    raw_args: &'msg str,
 }
 
 impl<'msg> Command<'msg> {
     pub fn parse(prefix: &str, msg: &'msg str) -> Option<Self> {
         let msg = msg.trim();
-        let mut args = msg.split_whitespace();
-        let name = args.next()
-            .filter(|name| name.starts_with(prefix))
-            .map(|name| &name[prefix.len()..])?;
+
+        if !msg.starts_with(prefix) {
+            return None;
+        }
+
+        let msg = &msg[prefix.len()..];
+        let mut parts = msg.splitn(2, ' ');
+        let name = parts.next()?;
+        let raw_args = parts.next().unwrap_or("");
 
         Some(Command {
             name: name,
-            args: args.collect(),
+            args: raw_args.split_whitespace().collect(),
+            raw_args,
         })
     }
 
@@ -23,5 +30,9 @@ impl<'msg> Command<'msg> {
     
     pub fn args(&self) -> &[&'msg str] {
         &self.args
+    }
+
+    pub fn raw_args(&self) -> &'msg str {
+        &self.raw_args
     }
 }
